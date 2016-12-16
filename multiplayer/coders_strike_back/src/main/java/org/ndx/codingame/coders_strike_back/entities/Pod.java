95e64a44c81537f30ddc3efb4625989003055a5c
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2016-12-16 13:07:42 +0100

    Oops, il manquait un champ public

--- multiplayer/coders_strike_back/src/main/java/org/ndx/codingame/coders_strike_back/entities/Pod.java
+++ multiplayer/coders_strike_back/src/main/java/org/ndx/codingame/coders_strike_back/entities/Pod.java
@@ -12,33 +12,33 @@ import org.ndx.codingame.lib2d.shapes.Line;
 
 public class Pod {
 
-	Position position;
-	private ContinuousPoint speed;
-	private int podAngle;
-	private int nextCheckpointId;
+	public Position position;
+	private final ContinuousPoint speed;
+	private final int podAngle;
+	private final int nextCheckpointId;
 
-	public Pod(Position continuousPoint, ContinuousPoint continuousPoint2, int angle, int nextInt) {
-		this.position = continuousPoint;
-		this.speed = continuousPoint2;
-		this.podAngle = angle;
-		this.nextCheckpointId = nextInt;
+	public Pod(final Position continuousPoint, final ContinuousPoint continuousPoint2, final int angle, final int nextInt) {
+		position = continuousPoint;
+		speed = continuousPoint2;
+		podAngle = angle;
+		nextCheckpointId = nextInt;
 	}
 
-	public String build(List<Position> CHECKPOINTS, Position previousPosition, Collection<Line> enemyDirections, boolean canBoost, boolean canShield) {
+	public String build(final List<Position> CHECKPOINTS, final Position previousPosition, final Collection<Line> enemyDirections, final boolean canBoost, final boolean canShield) {
 		System.err.println("building strategy for "+this);
-		Deque<Position> targets = new ArrayDeque<>();
+		final Deque<Position> targets = new ArrayDeque<>();
 		for (int index = 0; index < CHECKPOINTS.size(); index++) {
 			targets.add(CHECKPOINTS.get((nextCheckpointId+index)%CHECKPOINTS.size()));
 		}
-		Position destination = targets.getFirst();
-		int nextCheckpointDist = (int) position.distance2To(destination);
+		final Position destination = targets.getFirst();
+		final int nextCheckpointDist = (int) position.distance2To(destination);
 		int angle = 0;
-		Line direction = new Line(position, destination);
+		final Line direction = new Line(position, destination);
 		angle = (int) (podAngle-direction.angle());
 		if(angle>180) {
 			angle = angle-360;
 		}
-		Trajectory builder = Player.using().position(position)
+		final Trajectory builder = Player.using().position(position)
 				.previous(previousPosition)
 				.targets(targets)
 				.distance(nextCheckpointDist)
