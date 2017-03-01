5b3bf7876f2bb5999259646aa3715e4937eddec8
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2017-03-01 08:36:36 +0100

    Une tentative avec tri selon une production linéaire. Ca devrait me permettre des gains petits, mais rapides ...

--- multiplayer/ghost-in-the-cell/src/main/java/org/ndx/codingame/ghostinthecell/entities/Transport.java
+++ multiplayer/ghost-in-the-cell/src/main/java/org/ndx/codingame/ghostinthecell/entities/Transport.java
@@ -34,10 +34,39 @@ public class Transport extends Element {
 			return score(o1).compareTo(score(o2));
 		}
 
-		private Integer score(final Edge edge) {
+		private Float score(final Edge edge) {
 			final float production = edge.destination.getProperty(Factory.PRODUCTION);
 			final float distance = edge.getProperty(Transport.DISTANCE);
-			return (int) (production*production/distance);
+			/* with linear values, it gives the following array
+(D)|____ production_____|
+	1		2		3
+1	1		2		3
+2	0,50	1,00	1,50
+3	0,33	0,67	1,00
+4	0,25	0,50	0,75
+5	0,20	0,40	0,60
+6	0,17	0,33	0,50
+7	0,14	0,29	0,43
+8	0,13	0,25	0,38
+9	0,11	0,22	0,33
+
+while quadratic production gives
+
+(D)|____ production_____|
+	1		2		3
+1	1,00	4,00	9,00
+2	0,50	2,00	4,50
+3	0,33	1,33	3,00
+4	0,25	1,00	2,25
+5	0,20	0,80	1,80
+6	0,17	0,67	1,50
+7	0,14	0,57	1,29
+8	0,13	0,50	1,13
+9	0,11	0,44	1,00
+
+			 */
+			// we will try with linear production gain ...
+			return production/distance;
 		}
 		
 	};
