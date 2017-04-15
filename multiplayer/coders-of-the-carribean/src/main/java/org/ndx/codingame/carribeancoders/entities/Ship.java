5c0217b803f9ee216667447f1c72c8a1c631fe33
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2017-04-15 16:57:04 +0200

    Une petite tentative de faire du tir optimisé ... je ne suis pas sûr que ça marche tant que ça, mais il faut bien le tenter, non ?

--- multiplayer/coders-of-the-carribean/src/main/java/org/ndx/codingame/carribeancoders/entities/Ship.java
+++ multiplayer/coders-of-the-carribean/src/main/java/org/ndx/codingame/carribeancoders/entities/Ship.java
@@ -1,5 +1,7 @@
 package org.ndx.codingame.carribeancoders.entities;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -8,9 +10,26 @@ import org.ndx.codingame.carribeancoders.actions.ShootAt;
 import org.ndx.codingame.carribeancoders.playground.Playfield;
 import org.ndx.codingame.gaming.actions.Action;
 import org.ndx.codingame.lib2d.base.AbstractPoint;
+import org.ndx.codingame.lib2d.discrete.Direction;
 import org.ndx.codingame.lib2d.discrete.DiscretePoint;
 
 public class Ship extends Entity {
+	private static final List<Direction> EVEN_OFFSETS = Arrays.asList(
+			new Direction(1, 0, "right"),
+			new Direction(0, -1, "top right"),
+			new Direction(1, -1, "top left"),
+			new Direction(-1, 0, "left"),
+			new Direction(-1, 1, "down left"),
+			new Direction(0, 1, "down right")
+			);
+	private static final List<Direction> ODD_OFFSETS = Arrays.asList(
+			new Direction(1, 0, "right"),
+			new Direction(1, -1, "top right"),
+			new Direction(0, 1, "top left"),
+			new Direction(-1, 0, "left"),
+			new Direction(0, 1, "down left"),
+			new Direction(1, 1, "down right")
+			);
 	public final int speed;
 	public final int rum;
 	public final int orientation;
@@ -43,24 +62,42 @@ public class Ship extends Entity {
 			nearestEnemyPosition = enemiesByDistance.firstKey();
 		}
 		if(nearestBarrelPosition==null) {
-			return attackEnemy(nearestEnemyPosition);
+			return attackEnemy(nearestEnemyPosition, enemiesByDistance.get(nearestEnemyPosition));
 		} else if(rum<55) {
 			return new MoveTo(nearestBarrelPosition);
 		} else if(rum>80) {
-			return attackEnemy(nearestEnemyPosition);
+			return attackEnemy(nearestEnemyPosition, enemiesByDistance.get(nearestEnemyPosition));
 		} else {
 			return new MoveTo(nearestBarrelPosition);
 		}
 		
 	}
-	private Action attackEnemy(final DiscretePoint nearestEnemyPosition) {
+	private Action attackEnemy(final DiscretePoint nearestEnemyPosition, final Ship ship) {
 		if(nearestEnemyPosition.distance2To(position)<5) {
-			return new ShootAt(nearestEnemyPosition);
+			// Take in account enemy speed to aim the correct position
+			// This code is lame, and I feel bad for it, but I try it before diving into hex grid storage (which is quite complex)
+			return shootAt(nearestEnemyPosition, ship.orientation, ship.speed);
 		} else {
 			return new MoveTo(nearestEnemyPosition);
 		}
 	}
 	
+	private Action shootAt(final DiscretePoint nearestEnemyPosition, final int orientation2, final int speed2) {
+		if(speed2>0) {
+			List<Direction> OFFSETS = null;
+			switch(nearestEnemyPosition.y%2) {
+			case 0:
+				OFFSETS = EVEN_OFFSETS;
+				break;
+			case 1:
+				OFFSETS = ODD_OFFSETS;
+				break;
+			}
+			return shootAt(OFFSETS.get(orientation2).move(nearestEnemyPosition), orientation2, speed2-1);
+		} else {
+			return new ShootAt(nearestEnemyPosition);
+		}
+	}
 	@Override
 	public StringBuilder toUnitTestConstructor(final String multilinePrefix) {
 		final StringBuilder returned = super.toUnitTestConstructorPrefix(multilinePrefix);
