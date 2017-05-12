ac5ee92b1fa41ab26c3a324f558f3722c703d337
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2017-05-12 17:51:21 +0200

    Préparation de code4life

---
+++ multiplayer/code4life/src/main/java/org/ndx/codingame/code4life/Player.java
@@ -0,0 +1,8 @@
+package org.ndx.codingame.code4life;
+
+/**
+ * Auto-generated code below aims at helping you parse the standard input
+ * according to the problem statement.
+ **/
+public class Player {
+}
---
+++ multiplayer/code4life/src/test/java/org/ndx/codingame/code4life/InGameTest.java
@@ -0,0 +1,14 @@
+package org.ndx.codingame.code4life;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.Test;
+
+public class InGameTest {
+
+	@Test
+	public void test() {
+		assertThat(true).isFalse();
+	}
+
+}
