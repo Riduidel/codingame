90a518a670b539e27a0ccb17ab9b5b3ec3e1eafd
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2018-06-18 21:02:17 +0200

    Et enfin, enfin, j'arrive à générer le log des opérations en tour par tour. Je vais pouvoir avancer !

--- multiplayer/code_of_kutulu/src/main.rs
+++ multiplayer/code_of_kutulu/src/main.rs
@@ -170,9 +170,9 @@ impl Filler {
 impl PlaygroundVisitor<Static, String> for Filler {
     fn start_visit_playground(&mut self, visited:&Playground<Static>) {
         self.text.push_str(&self.prefix);
-        self.text.push_str(&format!("let tested = Playground::new({}, {});\n", visited.width, visited.height).to_owned());
+        self.text.push_str(&format!("let playground = Playground::new({}, {});\n", visited.width, visited.height).to_owned());
         self.text.push_str(&self.prefix);
-        self.text.push_str(&format!("tested.fill(vec![\n"));
+        self.text.push_str(&format!("playground.fill(vec![\n"));
     }
     fn end_visit_playground(&mut self, visited:&Playground<Static>) -> String {
         self.text.push_str(&self.prefix);
@@ -187,16 +187,140 @@ impl PlaygroundVisitor<Static, String> for Filler {
         self.text.push_str(&content.to_string());
     }
     fn end_visit_row(&mut self, line_index:usize, line:&Vec<Static>) {
-        self.text.push_str("\",");
-        if line_index<self.height {
-            self.text.push_str("\n");
+        self.text.push_str("\"");
+        if line_index<self.height-1 {
+        self.text.push_str(",");
         }
+        self.text.push_str("\n");
     }
 }
 
 impl ToUnitTest for Playground<Static> {
     fn to_unit_test(&self, prefix:&str) -> String {
         return Filler::on(self, prefix);
+    }
+}
+
+#[derive(Clone)]
+struct Game {
+    playground:Playground<Static>,
+    sanity_loss_lonely:i32,
+    sanity_loss_group:i32,
+    wanderer_spawn_time:i32,
+    wanderer_life_time:i32
+}
+
+impl Game {
+    fn new(playground:Playground<Static>, sanity_loss_lonely:i32, sanity_loss_group:i32, wanderer_spawn_time:i32, wanderer_life_time:i32) -> Game {
+        return Game {
+            playground: playground,
+            sanity_loss_lonely: sanity_loss_lonely,
+            sanity_loss_group: sanity_loss_group,
+            wanderer_spawn_time: wanderer_spawn_time,
+            wanderer_life_time: wanderer_life_time
+        }
+    }
+}
+
+impl ToUnitTest for Game {
+    fn to_unit_test(&self, prefix:&str) -> String {
+        let mut returned = "".to_string();
+        returned.push_str(&self.playground.to_unit_test(prefix));
+        returned.push_str(&prefix);
+        returned.push_str(
+            &format!(
+                "let game = Game::new(playground, {}, {}, {}, {});\n",
+                self.sanity_loss_lonely,
+                self.sanity_loss_group,
+                self.wanderer_spawn_time,
+                self.wanderer_life_time
+            ));
+        return returned;
+    }
+}
+
+enum EntityType {
+    EXPLORER,
+    WANDERER
+}
+impl From<String> for EntityType {
+    fn from(item:String) -> Self {
+        match item.as_ref() {
+            "EXPLORER" => EntityType::EXPLORER,
+            _ => EntityType::WANDERER
+        }
+    }
+}
+
+impl ToString for EntityType {
+    fn to_string(&self) -> String {
+        match *self {
+            EntityType::EXPLORER => "EXPLORER".to_string(),
+            EntityType::WANDERER => "WANDERER".to_string()
+        }
+    }
+}
+
+struct Entity {
+    entity_type:EntityType,
+    id:i32,
+    x:i32,
+    y:i32,
+    ///Explorer: sanity | Spawning minion: time before spawn | Wanderer: time before being recalled
+    param0:i32,
+    /// Explorer: ignore for this league | Minion: Current state amongst those -> SPAWNING = 0 , WANDERING = 1
+    param1:i32,
+    /// Explorer: ignore for this league | Minion: id  of the explorer targeted by this minion. -1 if no target (occurs only on spawn)
+    param2:i32
+}
+
+impl ToUnitTest for Entity {
+    fn to_unit_test(&self, prefix:&str) -> String {
+        let mut returned = "".to_string();
+        returned.push_str(
+            &format!("Entity {{ entity_type:{}, id:{}, x:{}, y:{}, param0:{}, param1:{}, param2:{} }}",
+                self.entity_type.to_string(),
+                self.id,
+                self.x,
+                self.y,
+                self.param0,
+                self.param1,
+                self.param2
+            ));
+        return returned;
+    }
+}
+
+struct Round {
+    game: Game,
+    entities:Vec<Entity>
+}
+
+impl Round {
+    fn new(game:Game, entities:Vec<Entity>) -> Round {
+        return Round { game: game, entities: entities };
+    }
+}
+impl ToUnitTest for Round {
+    fn to_unit_test(&self, prefix:&str) -> String {
+        let mut returned = "".to_string();
+        returned.push_str(&self.game.to_unit_test(prefix));
+        returned.push_str(prefix);
+        returned.push_str("let entities = vec![\n");
+        for (index, entity) in self.entities.iter().enumerate() {
+            returned.push_str(prefix);
+            returned.push_str("\t");
+            returned.push_str(&entity.to_unit_test(prefix));
+            if index<self.entities.len()-1 {
+                returned.push_str(",");
+            }
+            returned.push_str("\n");
+        }
+        returned.push_str(prefix);
+        returned.push_str("\t];\n");
+        returned.push_str(prefix);
+        returned.push_str("let round = Round::new(game, entities);");
+        return returned;
     }
 }
 
@@ -227,12 +351,14 @@ fn main() {
     let sanity_loss_group = parse_input!(inputs[1], i32); // how much sanity you lose every turn when near another player, always 1 until wood 1
     let wanderer_spawn_time = parse_input!(inputs[2], i32); // how many turns the wanderer take to spawn, always 3 until wood 1
     let wanderer_life_time = parse_input!(inputs[3], i32); // how many turns the wanderer is on map after spawning, always 40 until wood 1
+    let game = Game::new(playground, sanity_loss_lonely, sanity_loss_group, wanderer_spawn_time, wanderer_life_time);
 
     // game loop
     loop {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let entity_count = parse_input!(input_line, i32); // the first given entity corresponds to your explorer
+        let mut entities:Vec<Entity> = vec![];
         for i in 0..entity_count as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
@@ -244,12 +370,23 @@ fn main() {
             let param_0 = parse_input!(inputs[4], i32);
             let param_1 = parse_input!(inputs[5], i32);
             let param_2 = parse_input!(inputs[6], i32);
+            entities.push(Entity {
+                entity_type: EntityType::from(entity_type),
+                id: id,
+                x: x,
+                y: y,
+                param0: param_0,
+                param1: param_1,
+                param2: param_2
+            })
         }
-
+        // One new reference of round is generated each ... well ... round, so game is no more owned
+        // I guess there should be a borrow thing
+        let mut round = Round::new(game.clone(), entities);
         // Write an action using println!("message...");
         // To debug: eprintln!("Debug message...");
 
-    	eprintln!("{}", playground.can_compute_at());
+    	eprintln!("{}", round.can_compute_at());
         println!("WAIT"); // MOVE <x> <y> | WAIT
     }
 }
--- multiplayer/code_of_kutulu/src/test.rs
+++ multiplayer/code_of_kutulu/src/test.rs
@@ -3,5 +3,5 @@ fn simple_playground_work() {
     let mut playground = super::Playground::new(2, 2);
     playground.fill(vec!["# ".to_string(), "#w".to_string()]);
     assert_eq!(super::Filler::on(&playground, "\t"),
-        "\tlet tested = Playground::new(2, 2);\n\ttested.fill(vec![\n\t\t\"# \",\n\t\t\"#w\",\n\t\t]);\n");
+        "\tlet playground = Playground::new(2, 2);\n\tplayground.fill(vec![\n\t\t\"# \",\n\t\t\"#w\"\n\t\t]);\n");
 }
