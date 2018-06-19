c4f168edc07af6eb756c0c47b31ba2974119dcdb
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2018-06-19 21:31:12 +0200

    Avec des jolis tests, c'est mieux ... Et surtout, pendant que j'écris, ça passe en wood 2 !

--- multiplayer/code_of_kutulu/src/test.rs
+++ multiplayer/code_of_kutulu/src/test.rs
@@ -1,3 +1,5 @@
+use spectral::prelude::*;
+
 use Playground;
 use Game;
 use Round;
@@ -47,41 +49,5 @@ fn just_go_away_when_wanderer_come() {
 		Entity { entity_type:WANDERER, id:13, x:2, y:13, param0:2, param1:0, param2:-1 }
 		];
 	let round = Round::new(game, entities);
-    assert!(round.compute().starts_with("MOVE 8 6"));
-}
-
-#[test]
-fn can_compute_at_1529433206709() {
-	let mut playground = Playground::new(20, 16, Static::Empty);
-	playground.fill(vec![
-		"####################".to_string(),
-		"##                ##".to_string(),
-		"# w### ###### ###w #".to_string(),
-		"# #    #    #    # #".to_string(),
-		"# # ## # ## # ## # #".to_string(),
-		"#   ##   ##   ##   #".to_string(),
-		"# #    #    #    # #".to_string(),
-		"# ### ## ## ## ### #".to_string(),
-		"# ### ## ## ## ### #".to_string(),
-		"# #    #    #    # #".to_string(),
-		"#   ##   ##   ##   #".to_string(),
-		"# # ## # ## # ## # #".to_string(),
-		"# #    #    #    # #".to_string(),
-		"# w### ###### ###w #".to_string(),
-		"##                ##".to_string(),
-		"####################".to_string()
-		]);
-	let game = Game::new(playground, 3, 1, 3, 40);
-	let entities = vec![
-		Entity { entity_type:EXPLORER, id:0, x:8, y:3, param0:247, param1:2, param2:3 },
-		Entity { entity_type:EXPLORER, id:1, x:11, y:8, param0:247, param1:2, param2:3 },
-		Entity { entity_type:EXPLORER, id:2, x:8, y:5, param0:247, param1:2, param2:3 },
-		Entity { entity_type:EXPLORER, id:3, x:11, y:7, param0:247, param1:2, param2:3 },
-		Entity { entity_type:WANDERER, id:4, x:2, y:1, param0:40, param1:1, param2:-1 },
-		Entity { entity_type:WANDERER, id:5, x:17, y:1, param0:40, param1:1, param2:-1 },
-		Entity { entity_type:WANDERER, id:6, x:2, y:14, param0:40, param1:1, param2:-1 },
-		Entity { entity_type:WANDERER, id:7, x:17, y:14, param0:40, param1:1, param2:-1 }
-		];
-	let round = Round::new(game, entities);
-	assert!(round.compute()!="MOVE 8 3 Score is 5");
+    assert_that!(&round.compute()).starts_with("MOVE 8 6");
 }
