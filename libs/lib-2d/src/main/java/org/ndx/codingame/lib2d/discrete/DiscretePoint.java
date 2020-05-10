e40a03f1188b2b2447aeb2325e968dd97e07ec9a
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2020-05-10 18:05:24 +0200

    Optimized discrete point hashcode and equals

--- libs/lib-2d/src/main/java/org/ndx/codingame/lib2d/discrete/DiscretePoint.java
+++ libs/lib-2d/src/main/java/org/ndx/codingame/lib2d/discrete/DiscretePoint.java
@@ -38,4 +38,27 @@ public class DiscretePoint extends AbstractPoint implements PointBuilder<Discret
 	public DiscretePoint build(final double x, final double y) {
 		return new DiscretePoint((int)x, (int)y);
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DiscretePoint other = (DiscretePoint) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
 }
