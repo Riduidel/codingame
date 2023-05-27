9f998d16c6e2bcc87394bec576d245b785881590
Nicolas Delsaux <nicolas.delsaux@gmx.fr>
2023-05-27 18:21:18 +0200

    Refactoring code to have only a simple main method

--- multiplayer/spring-challenge-2023/src/lib.rs
+++ multiplayer/spring-challenge-2023/src/lib.rs
@@ -44,18 +44,15 @@ pub struct Cell {
     initial_resources:i32,
     resources:i32,
     my_ants:i32,
-    enemy_ants:i32
+    opponent_ants:i32,
+    neighbours: Vec<i32>
 }
 
 impl Cell {
-    pub fn with_resources(&mut self, resources:i32)->&mut Self {
-        self.resources = resources; self
-    }
-    pub fn with_my_ants(&mut self, my_ants:i32)->&mut Self {
-        self.my_ants = my_ants; self
-    }
-    pub fn with_enemy_ants(&mut self, enemy_ants:i32)->&mut Self {
-        self.enemy_ants = enemy_ants; self
+    pub fn update(&mut self, resources:i32, my_ants:i32, opponent_ants:i32) {
+        self.resources = resources;
+        self.my_ants = my_ants;
+        self.opponent_ants = opponent_ants;
     }
 }
 
@@ -65,36 +62,52 @@ pub struct Player {
      */
     bases:Vec<usize>
 }
+
+pub struct Playground {
+    cells:Vec<Cell>
+}
+
+impl Playground {
+    fn parse_initial_playground()->Playground {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let number_of_cells = parse_input!(input_line, usize); // amount of hexagonal cells in this map
+        let mut cells:Vec<Cell> = Vec::with_capacity(number_of_cells as usize);
+        for i in 0..number_of_cells {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let cell_type = parse_input!(inputs[0], i32); // 0 for empty, 1 for eggs, 2 for crystal
+            let initial_resources = parse_input!(inputs[1], i32); // the initial amount of eggs/crystals on this cell
+            let neigh_0 = parse_input!(inputs[2], i32); // the index of the neighbouring cell for each direction
+            let neigh_1 = parse_input!(inputs[3], i32);
+            let neigh_2 = parse_input!(inputs[4], i32);
+            let neigh_3 = parse_input!(inputs[5], i32);
+            let neigh_4 = parse_input!(inputs[6], i32);
+            let neigh_5 = parse_input!(inputs[7], i32);
+            cells.push(Cell {
+                cell_index:i,
+                cell_content: unsafe { ::std::mem::transmute(cell_type) },
+                initial_resources: initial_resources,
+                resources: 0,
+                my_ants: 0,
+                opponent_ants: 0,
+                neighbours: vec!(neigh_0, neigh_1, neigh_2, neigh_3, neigh_4, neigh_5)
+            });
+        }
+        Playground {
+            cells: cells
+        }
+    
+    }
+}
+
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
 fn main() {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    let number_of_cells = parse_input!(input_line, usize); // amount of hexagonal cells in this map
-    let mut cells:Vec<Cell> = Vec::with_capacity(number_of_cells as usize);
-    for i in 0..number_of_cells {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let cell_type = parse_input!(inputs[0], i32); // 0 for empty, 1 for eggs, 2 for crystal
-        let initial_resources = parse_input!(inputs[1], i32); // the initial amount of eggs/crystals on this cell
-        let neigh_0 = parse_input!(inputs[2], i32); // the index of the neighbouring cell for each direction
-        let neigh_1 = parse_input!(inputs[3], i32);
-        let neigh_2 = parse_input!(inputs[4], i32);
-        let neigh_3 = parse_input!(inputs[5], i32);
-        let neigh_4 = parse_input!(inputs[6], i32);
-        let neigh_5 = parse_input!(inputs[7], i32);
-        cells.push(Cell {
-            cell_index:i,
-            cell_content: unsafe { ::std::mem::transmute(cell_type) },
-            initial_resources: initial_resources,
-            resources: 0,
-            my_ants: 0,
-            enemy_ants: 0
-        });
-    }
+    let mut playground = Playground::parse_initial_playground();
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let number_of_bases = parse_input!(input_line, i32);
@@ -121,22 +134,20 @@ fn main() {
 
     // game loop
     loop {
-        for cell_index in 0..number_of_cells as usize {
+        for cell_index in 0..playground.cells.len() as usize {
             let mut input_line = String::new();
             io::stdin().read_line(&mut input_line).unwrap();
             let inputs = input_line.split(" ").collect::<Vec<_>>();
             let resources = parse_input!(inputs[0], i32); // the current amount of eggs/crystals on this cell
             let my_ants = parse_input!(inputs[1], i32); // the amount of your ants on this cell
             let opp_ants = parse_input!(inputs[2], i32); // the amount of opponent ants on this cell
-            cells.get_mut(cell_index).unwrap()
-                .with_resources(resources)
-                .with_my_ants(my_ants)
-                .with_enemy_ants(opp_ants);
+            playground.cells.get_mut(cell_index).unwrap()
+                .update(resources, my_ants, opp_ants);
 
         }
 
         // Find all cells having resources
-        let targets:Vec<&Cell> = cells
+        let targets:Vec<&Cell> = playground.cells
             .iter()
             .filter(|cell| cell.resources>0)
             .collect();
@@ -147,7 +158,7 @@ fn main() {
         let actions:Vec<Action> = my_player.bases
             .iter()
             .flat_map(|base_index| {
-                let base_cell = &cells[*base_index];
+                let base_cell = &playground.cells[*base_index];
                 let actions:Vec<Action> = targets.iter()
                     .map(|cell| {
                         Action::LINE { start_index: base_index.clone(), end_index: cell.cell_index.clone(), strenght: 1}
